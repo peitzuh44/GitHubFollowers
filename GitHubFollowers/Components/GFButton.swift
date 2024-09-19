f42c6d789d19a6e2a87a6036f46e5d0ff8a15099
Pei <peitzuh44@berkeley.edu>
2024-09-19 19:53:03 +0800

    Create reusable components - GDButton and GFTextfield

--- GitHubFollowers/Components/GFButton.swift
+++ GitHubFollowers/Components/GFButton.swift
@@ -7,14 +7,39 @@
 
 import UIKit
 
-class GFButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+// Creating Reusable Button
+
+// Creating a GFButton on top of the UIButton
+class GFButton: UIButton {
+    
+    /// Step 1 - Create an init with a frame(so we can tell xcode how big the button is). We use override to customize our own button. In this case, we are creating "GFButton" which is a subclass of UIButton. Use "super" to call the default button "UIButton"
+    override init(frame: CGRect) { /// This line init the parent one "UIButton". So we got everything that apple built inside
+        super.init(frame: frame)
+        configure()
     }
-    */
+    
+    
+    // This is how to handle initialization via storyboard.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    // MARK: UI of the Button
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
 
 }
+
---
+++ GitHubFollowers/Components/GFTextField.swift
@@ -0,0 +1,42 @@
+//
+//  GFTextField.swift
+//  GitHubFollowers
+//
+//  Created by Pei-Tzu Huang on 2024/9/19.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        placeholder = "Enter a user name"
+        
+    }
+    
+}
