d9564c9326dc1b2ef514e71cfed7b7b61f99c5a5
Pei <peitzuh44@berkeley.edu>
2024-09-19 19:51:05 +0800

    Delete storyboard

---
+++ GitHubFollowers/Components/GFButton.swift
@@ -0,0 +1,20 @@
+//
+//  GFButton.swift
+//  GitHubFollowers
+//
+//  Created by Pei-Tzu Huang on 2024/9/19.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
