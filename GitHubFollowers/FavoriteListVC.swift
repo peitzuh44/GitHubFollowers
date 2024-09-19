e57ad8608bb3c020576b8453be75d0a96b867ade
Pei <peitzuh44@berkeley.edu>
2024-09-19 19:52:19 +0800

    Set up navigationController and TabBarController

---
+++ GitHubFollowers/FavoriteListVC.swift
@@ -0,0 +1,22 @@
+//
+//  FavoriteListVC.swift
+//  GitHubFollowers
+//
+//  Created by Pei-Tzu Huang on 2024/9/19.
+//
+
+import UIKit
+
+class FavoriteListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+
+    }
+    
+
+    
+
+}
---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,21 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by Pei-Tzu Huang on 2024/9/19.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+
+    }
+    
+
+   
+
+}
