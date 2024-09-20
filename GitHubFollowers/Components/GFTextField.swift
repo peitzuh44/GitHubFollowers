46eac58a9e13fbe99b1b5ff0d07415c3fd0524f6
Pei <peitzuh44@berkeley.edu>
2024-09-20 13:06:31 +0800

    Configure UI for SearchVC

--- GitHubFollowers/Components/GFTextField.swift
+++ GitHubFollowers/Components/GFTextField.swift
@@ -21,18 +21,15 @@ class GFTextField: UITextField {
     
     private func configure() {
         translatesAutoresizingMaskIntoConstraints = false
-        
         layer.cornerRadius = 10
         layer.borderWidth = 2
         layer.borderColor = UIColor.systemGray4.cgColor
-        
         textColor = .label
         tintColor = .label
         textAlignment = .center
         font = UIFont.preferredFont(forTextStyle: .title2)
         adjustsFontSizeToFitWidth = true
         minimumFontSize = 12
-        
         backgroundColor = .tertiarySystemBackground
         autocorrectionType = .no
         placeholder = "Enter a user name"
--- GitHubFollowers/SearchVC.swift
+++ GitHubFollowers/SearchVC.swift
@@ -8,11 +8,73 @@
 import UIKit
 
 class SearchVC: UIViewController {
+    
+    let logoImageView = UIImageView()
+    let usernameTextField = GFTextField()
+    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
+        configureLogoImageView()
+        configureTextField()
+        configureButton()
 
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true // hide the
+    }
+    
+    // Steps for configuring:
+    /* 1. add the subview to view
+       2. set autolayout to false
+       3. set the image name
+       4. set the constraints
+     */
+    
+    func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "github")
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            // Use safe area layout guide so it starts the padding below the safearea.
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        
+    }
+    
+    func configureTextField() {
+        view.addSubview(usernameTextField)
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 50),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 55)
+            
+            // In this case, you are not setting the width anchor. Instead, use the leading and trailing constraints to set a dynamic width on the textfield.
+
+        ])
+    }
+    
+    func configureButton() {
+        view.addSubview(callToActionButton)
+        callToActionButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 50),
+            callToActionButton.heightAnchor.constraint(equalToConstant: 55)
+        ])
     }
     
 
